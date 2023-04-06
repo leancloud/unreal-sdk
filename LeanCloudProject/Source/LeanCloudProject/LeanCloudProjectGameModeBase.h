efa937fea7de64c6fbe414d1f86cb180433e7498
harryphone <harryphone@163.com>
2023-04-06 17:36:15 +0800

    feat: Create new project

---
+++ LeanCloudProject/Source/LeanCloudProject/LeanCloudProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LeanCloudProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEANCLOUDPROJECT_API ALeanCloudProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
